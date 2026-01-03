8e32693506d5af28a3fc412eb88696e7a55b83a0
“Denis <denis.budau@edu.unito.it>
2026-01-03 16:18:24 +0100

    start main server TWEB

---
+++ solution/main server/routes/anime.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const AXIOS = require('axios');
+const anime_controller = require('../controllers/animeController');
+
+// ROUTER PER AVERE TUTTI GLI ANIME
+async function getAnime(req, res) {
+    try {
+        // Effettua una richiesta GET per ottenere tutti i film
+        const response = await AXIOS.get(`http://localhost:8080/anime/getAnimes`);
+
+    } catch (error) {
+        // Log dell'errore nel caso in cui la richiesta fallisca
+        console.error('Errore nel recupero dei film dal server Spring:', error);
+        res.status(500).render('pages/error', { message: 'Dati non disponibili' });
+    }
+}
+
+// ROUTER PER AVERE TUTTE LE INFO DI UN ANIME SPECIFICO,
+router.get('/anime_info', anime_controller.getAnimeInfo);
+
+
+module.exports = router;
---
+++ solution/main server/routes/userProfile.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const userProfileController = require('../controllers/userProfileController');
+
+// ROUTER PER AVERE TUTTI GLI UTENTI
+router.get('/userProfile',async (req, res) => {
+    try {
+        // Chiamata al secondo server per ottenere i dati degli utenti
+        const response = await axios.get('http://localhost:3001/userProfile');
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+// ROUTER PER AVERE TUTTE LE INFO DI UN UTENTE SPECIFICO, RIFARE CON AWAIT DENTRO ROUTER NON IN CONTROLLER
+router.get('/userProfile/:name', async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+})
+
+module.exports = router;
